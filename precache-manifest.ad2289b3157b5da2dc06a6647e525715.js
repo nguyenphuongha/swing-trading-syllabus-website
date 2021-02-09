4d99c21c21569e688b29aecf1fcc55de10ad57d1
Phuong Bui Ngoc <ngocphuongpro98@gmail.com>
2021-02-10 05:40:14 +0700

    Updates

---
+++ precache-manifest.ad2289b3157b5da2dc06a6647e525715.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00c9fa8e945494160b8d3a49388ccb8",
+    "url": "/swing-trading-syllabus-website/index.html"
+  },
+  {
+    "revision": "9b75ac28f6c531fda55e",
+    "url": "/swing-trading-syllabus-website/static/css/main.e9b42763.chunk.css"
+  },
+  {
+    "revision": "c684a36039c9462c4430",
+    "url": "/swing-trading-syllabus-website/static/js/2.f2e489e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/swing-trading-syllabus-website/static/js/2.f2e489e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b75ac28f6c531fda55e",
+    "url": "/swing-trading-syllabus-website/static/js/main.ba2f161b.chunk.js"
+  },
+  {
+    "revision": "ccca99fdb04e735d7587",
+    "url": "/swing-trading-syllabus-website/static/js/runtime-main.9b539669.js"
+  }
+]);
